0d49cb497a435796af6368a8a6e3c16fc6ba5090
sumoomus <cummbin0@gmail.com>
2023-05-21 19:18:40 +0300

    Homework_QA24_IlyaBobinin

---
+++ src/test/java/FileUpload.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class FileUpload {
+    private static final String URL = "https://the-internet.herokuapp.com/upload";
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void navigate() {
+        driver.get(URL);
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testFileUpload() {
+
+
+        driver.findElement(By.xpath("//input[@type='file']"))
+                .sendKeys(System.getProperty("user.dir") + "/src/test/resources/example.txt");
+        WebElement uploadButton = driver.findElement(By.id("file-submit"));
+        uploadButton.click();
+
+
+    }
+
+}
