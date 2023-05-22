8c61ed5504a635e3e0060fd03ce76bacf583bba3
sumoomus <cummbin0@gmail.com>
2023-05-22 22:59:56 +0300

    Homework_QA24_IlyaBobinin New code applied

--- src/test/java/FileUpload.java
+++ src/test/java/FileUpload.java
@@ -2,11 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.time.Duration;
 
 
@@ -40,6 +42,10 @@ public class FileUpload {
                 .sendKeys(System.getProperty("user.dir") + "/src/test/resources/example.txt");
         WebElement uploadButton = driver.findElement(By.id("file-submit"));
         uploadButton.click();
+
+        File uploadedFile = new File("/src/test/resources/example.txt");
+        String expectedFileName = uploadedFile.getName();
+        Assert.assertEquals("example.txt", expectedFileName);
 
 
     }
